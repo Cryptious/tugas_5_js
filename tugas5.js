ae80a43b398c8cd77f4706c8992151acfa4afd4f
Martino Wagey <martinowagey@gmail.com>
2019-07-22 16:53:29 +0700

    add tugas 5 JS

---
+++ tugas5.js
@@ -0,0 +1,14 @@
+function tugas5() {
+
+
+  var tugas5 = []
+
+  for (var i = 1; i <= 20; i++) {
+    if (i%3 === 0) {
+      tugas5.push(i)
+    }
+  }
+  return tugas5;
+}
+
+console.log(tugas5());
